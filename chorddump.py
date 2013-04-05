1c4db1defa0f08d160d1978b0963237c2aadcb84
Michael Hayes <michael.hayes@canterbury.ac.nz>
2013-04-06 11:59:44 +1300

    Add chord dumps

---
+++ chorddump.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""ChordPro chord dump"""
+
+from parser import *
+from chords_dump import *
+
+if __name__ == '__main__':
+    import codecs, argparse, sys
+
+    parser = argparse.ArgumentParser(description="Chordpro converter")
+
+    parser.add_argument('filename')
+
+    args = parser.parse_args()
+
+    file = codecs.open(args.filename, encoding='utf-8')
+    parsed = parse(file)
+
+    sys.stdout.write(show(parsed, ChordsDump, frets=True))
+
---
+++ chords_dump.py
@@ -0,0 +1,36 @@
+from chord import Chord
+from chords import Chords
+
+
+class ChordsDump(object):
+
+    def __init__(self, frets=False, gchord=True):
+        self._used_chords = []
+        self._chords = Chords()
+        self._frets = frets
+
+    def visit_define(self, text):
+        c = text.split(' ', 1)
+        chordname = c[0]
+
+        c = c[1].split(' ', 3)
+        if c[0] != 'base-fret' or c[2] != 'frets':
+            sys.stderr.write('Invalid define format: ' + text)
+
+        self._chords.add(Chord(chordname, c[3], c[1]))
+
+    def visit_line(self, chords):
+        line = ''
+        for chord, text in chords:
+            if not chord:
+                continue
+            if chord not in ('/', 'N.C.') and chord not in self._used_chords:
+                self._used_chords.append(chord)
+
+    def result(self):
+
+        if self._frets:
+            return '\n'.join([self._chords.find(chordname).format() for chordname in self._used_chords]) + '\n'
+
+        return ' '.join(self._used_chords) + '\n'
+
