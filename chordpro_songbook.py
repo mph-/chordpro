62b135a2e005be1aec2d36bc4aad949bd3d76eff
mph- <mph@thing3.(none)>
2013-04-06 21:41:59 +1300

    Remove format

--- chordpro_songbook.py
+++ chordpro_songbook.py
@@ -2,14 +2,15 @@
 """ChordPro Parser"""
 
 from songbook import Songbook
+from parser import *
+from tex_visitor import *
 
 if __name__ == '__main__':
-    import codecs, argparse, sys
+    import codecs, argparse, sys, os.path
 
     parser = argparse.ArgumentParser(description="Chordpro songbook")
 
     parser.add_argument('filename')
-    parser.add_argument('--format', default="ascii")
     parser.add_argument('--output', default="-")
 
     args = parser.parse_args()
@@ -17,5 +18,18 @@ if __name__ == '__main__':
     file = codecs.open(args.filename, encoding='utf-8')
     out = open(args.output, "w") if args.output != "-" else sys.stdout
 
+    dirname = os.path.dirname(args.filename)
+
     songbook = Songbook(file)
-    print songbook.songs
+
+    for song in songbook.songs:
+
+        file = codecs.open(os.path.join(dirname, song), encoding='utf-8') 
+        parsed = parse(file)
+
+        out.write(show(parsed, TexVisitor, template='song_template.tex'))
+        
+
+        
+
+
